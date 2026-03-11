1c3fc715ec38cfb57e2f8cee67fefcaecabfcbc8
Taahia23 <taahiatahsin@gmail.com>
2026-03-12 03:40:05 +0600

    using clean data classification report has been created

---
+++ models/train_logistic_regression.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+df = pd.read_csv("data/clean_dataset.csv")
+
+X = df["clean_text"]
+y = df["label"]
+
+vectorizer = TfidfVectorizer(max_features=5000)
+
+X_vectorized = vectorizer.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_vectorized, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression(max_iter=200)
+
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+print(classification_report(y_test, pred))
